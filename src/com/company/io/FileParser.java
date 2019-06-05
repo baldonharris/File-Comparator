67db43ff3d16581aa962637515f14a6c7d027cce
Harris C. Baldon <hbaldon93@gmail.com>
2019-06-05 17:48:03 +0800

    Read per line of csv instead of storing to array list

--- src/com/company/io/FileParser.java
+++ src/com/company/io/FileParser.java
@@ -4,11 +4,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.stream.Stream;
 
 public class FileParser
 {
+    private String fileName;
     private File file;
     private HashMap<String, ArrayList<String>> data;
     private String[] columnNames;
@@ -16,6 +20,7 @@ public class FileParser
 
     public FileParser(String fileName) throws IOException
     {
+        this.fileName = fileName;
         this.file = new File(fileName);
         this.readFile();
     }
@@ -26,21 +31,46 @@ public class FileParser
 
     public int getColumnCount() { return this.columnNames.length; }
 
-    public String getDataByColumnAndRow(String column, int row)
+    public String getDataByColumnAndRow(String column, int row) throws IOException, NullPointerException
     {
-        try {
-            return this.data.get(column).get(row);
-        } catch (Exception e) {
-            return null;
+        String line;
+        String[] rowData;
+        int columnIndex = 0;
+        try (Stream<String> lines = Files.lines(Paths.get(this.fileName))) {
+            line = lines.skip(row).findFirst().get();
         }
-    }
 
-    private void addToColumn(String column, String value)
-    {
-        ArrayList<String> temp = this.data.containsKey(column) ? this.data.get(column) : new ArrayList<>();
+        rowData = explodeString(line);
 
-        temp.add(value);
-        this.data.put(column, temp);
+        for (int c = 0; c < this.columnNames.length; c++) {
+            if (this.columnNames[c].equals(column)) {
+                columnIndex = c;
+                break;
+            }
+        }
+
+        return rowData[columnIndex];
+
+//        BufferedReader tempBr = new BufferedReader(new FileReader(this.file));
+//        String[] rowData;
+//        int columnIndex = 0;
+//
+//        for (int r = 0; r < (row + 1); r++) {
+//            tempBr.readLine();
+//        }
+//
+//        rowData = explodeString(tempBr.readLine());
+//
+//        for (int c = 0; c < this.columnNames.length; c++) {
+//            if (this.columnNames[c].equals(column)) {
+//                columnIndex = c;
+//                break;
+//            }
+//        }
+//
+//        tempBr.close();
+//
+//        return rowData[columnIndex];
     }
 
     private String[] explodeString(String line)
@@ -64,13 +94,12 @@ public class FileParser
 
             if (this.columnNames != null && !line.isEmpty()) {
                 this.rowCount++;
-                for (int i=0; i<lineData.length; i++) {
-                    this.addToColumn(this.columnNames[i], lineData[i]);
-                }
             } else {
                 this.columnNames = lineData;
             }
         }
+
+        br.close();
 
         System.out.println("FileParser: Reading of file success.\n\n");
     }
